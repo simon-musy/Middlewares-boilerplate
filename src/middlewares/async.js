5a69f560dc39bf56186a845c72507242a000ea3a
Simon Musy <simon.musy@nexthink.com>
2018-01-30 17:04:32 +0100

    Init

---
+++ src/middlewares/async.js
@@ -0,0 +1,10 @@
+export default function({ dispatch }) {
+    return next => action => {
+
+        if(!action.payload || !action.payload.then) {
+            return next(action);
+        }
+
+        action.payload.then(response => next({ ...action, payload: response}));
+    };
+}
